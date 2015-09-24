ff9426fb4c741edcc18a1a335de83f09acd523fc
thelaphone <student@physics-mp301-0.boisestate.edu>
2015-09-24 12:36:12 -0600

    from workshop 9/14/15

---
+++ Python examples/Python_example_sept24_15.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+
+
+y = np.sin(0)
+z = np.cos(0)
+
+print(y, z)
+
+
+def cosine(theta):
+    x = np.cos(theta)
+    return x
+
+print(os.getcwd())
+print(cosine(np.pi/2.0))
+
